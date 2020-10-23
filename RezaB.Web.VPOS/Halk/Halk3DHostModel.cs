3c8392f60f2b9772cc0b90d4d1ddfbf8b3d1c6d9
Onur Civanoğlu <onurcivanoglu@yandex.com>
2020-10-23 18:32:43 +0300

    test

--- RezaB.Web.VPOS/Halk/Halk3DHostModel.cs
+++ RezaB.Web.VPOS/Halk/Halk3DHostModel.cs
@@ -9,7 +9,7 @@ namespace RezaB.Web.VPOS.Halk
     public class Halk3DHostModel : Netspay3DHostModel
     {
         public override string ActionLink
-        {
+        { 
             get
             {
                 return @"https://sanalpos.halkbank.com.tr/fim/est3dgate";
