0ae69d1e14709da1fb69d3ffa36eba0a18c7ee95
RezaB30 <r.barzegaran@gmail.com>
2020-12-24 16:36:27 +0300

    V 1.0.1 Added get html form to VPOS3DHostModel base.

--- RezaB.Web.VPOS/VPOS3DHostHelper.cs
+++ RezaB.Web.VPOS/VPOS3DHostHelper.cs
@@ -15,46 +15,47 @@ namespace RezaB.Web.VPOS
         public static MvcHtmlString VPOS3DHostFormFor<TModel,TResult>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TResult>> expression) where TResult : VPOS3DHostModel
         {
             var metadata = ModelMetadata.FromLambdaExpression(expression, helper.ViewData);
-            var fieldName = ExpressionHelper.GetExpressionText(expression);
-            var fullName = helper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(fieldName);
-            var Model = metadata.Model;
+            //var fieldName = ExpressionHelper.GetExpressionText(expression);
+            //var fullName = helper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(fieldName);
+            var Model = metadata.Model as VPOS3DHostModel;
 
-            var properties = Model.GetType().GetProperties();
-            var validProps = new List<PropertyInfo>();
-            foreach (var property in properties)
-            {
-                var attrib = property.GetCustomAttribute<VPOSParameterAttribute>();
-                if (attrib != null)
-                    validProps.Add(property);
-            }
+            return Model.GetHtmlForm();
+            //var properties = Model.GetType().GetProperties();
+            //var validProps = new List<PropertyInfo>();
+            //foreach (var property in properties)
+            //{
+            //    var attrib = property.GetCustomAttribute<VPOSParameterAttribute>();
+            //    if (attrib != null)
+            //        validProps.Add(property);
+            //}
 
-            var actionLink = ((VPOS3DHostModel)Model).ActionLink;
+            //var actionLink = ((VPOS3DHostModel)Model).ActionLink;
 
-            TagBuilder form = new TagBuilder("form");
-            form.MergeAttributes(new Dictionary<string, string>() {
-                    { "action", actionLink },
-                    { "method", "post" },
-                    { "name", "payment" }
-                });
-            form.AddCssClass("vpos-redirect-container");
+            //TagBuilder form = new TagBuilder("form");
+            //form.MergeAttributes(new Dictionary<string, string>() {
+            //        { "action", actionLink },
+            //        { "method", "post" },
+            //        { "name", "payment" }
+            //    });
+            //form.AddCssClass("vpos-redirect-container");
 
-            form.InnerHtml = Messages.Redirecting;
+            //form.InnerHtml = Messages.Redirecting;
 
-            foreach (var property in validProps)
-            {
-                var currentValue = property.GetValue(Model);
-                TagBuilder hidden = new TagBuilder("input");
-                hidden.MergeAttribute("type", "hidden");
-                hidden.MergeAttribute("name", property.Name);
-                hidden.MergeAttribute("value", currentValue != null ? currentValue.ToString() : string.Empty);
-                form.InnerHtml += hidden.ToString(TagRenderMode.SelfClosing);
-            }
+            //foreach (var property in validProps)
+            //{
+            //    var currentValue = property.GetValue(Model);
+            //    TagBuilder hidden = new TagBuilder("input");
+            //    hidden.MergeAttribute("type", "hidden");
+            //    hidden.MergeAttribute("name", property.Name);
+            //    hidden.MergeAttribute("value", currentValue != null ? currentValue.ToString() : string.Empty);
+            //    form.InnerHtml += hidden.ToString(TagRenderMode.SelfClosing);
+            //}
 
-            TagBuilder script = new TagBuilder("script");
-            script.MergeAttribute("type", "text/javascript");
-            script.InnerHtml += "document.payment.submit();";
+            //TagBuilder script = new TagBuilder("script");
+            //script.MergeAttribute("type", "text/javascript");
+            //script.InnerHtml += "document.payment.submit();";
 
-            return new MvcHtmlString(form.ToString(TagRenderMode.Normal) + Environment.NewLine + script.ToString(TagRenderMode.Normal));
+            //return new MvcHtmlString(form.ToString(TagRenderMode.Normal) + Environment.NewLine + script.ToString(TagRenderMode.Normal));
         }
     }
 }
